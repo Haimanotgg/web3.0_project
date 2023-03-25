203dfa949241ff3a937d46bc021a69a585231d75
Haimanotgg <Haimanotgbb@gmail.com>
2023-03-25 13:05:08 +0300

    initial commit

--- Smart_contract/hardhat.config.js
+++ Smart_contract/hardhat.config.js
@@ -1,9 +1,14 @@
 // https://eth-goerli.g.alchemy.com/v2/eF4scgBB-QA_Oda7Y4Gu3MW_7I4XN0dN
 
 
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
+  networks : {
+    goerli : {
+      url : 'https://eth-goerli.g.alchemy.com/v2/eF4scgBB-QA_Oda7Y4Gu3MW_7I4XN0dN',
+      account : ['42a381928f67cb44e8872a38477be89c425e635e53cba5f70bab83d4499750ba']
+  }
+  }
 };
