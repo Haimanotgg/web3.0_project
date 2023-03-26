93d15cd9b4713400c3d41991aa712c8d2403ee7b
Haimanotgg <Haimanotgbb@gmail.com>
2023-03-26 04:27:13 +0300

    initial commit

--- Smart_contract/hardhat.config.js
+++ Smart_contract/hardhat.config.js
@@ -1,5 +1,6 @@
 // https://eth-goerli.g.alchemy.com/v2/eF4scgBB-QA_Oda7Y4Gu3MW_7I4XN0dN
 require('@nomiclabs/hardhat-waffle');
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.18",
