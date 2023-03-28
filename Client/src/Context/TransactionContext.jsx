984c087958917128fa6aafce846ddb5b9d791cc0
Haimanotgg <Haimanotgbb@gmail.com>
2023-03-28 12:33:43 +0300

    initial commit

--- Client/src/Context/TransactionContext.jsx
+++ Client/src/Context/TransactionContext.jsx
@@ -6,3 +6,22 @@ import { contractABI, contractAddress } from '../utils/Constants';
 export const TransactionContext = React.createContext();
 
 const { ethereum } = window;
+
+const createEthereumContract = () => {
+    const provider = new ethers.providers.Web3Provider(ethereum);
+    const signer = provider.getSigner();
+    const transactionsContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    console.log(
+        {signer,
+        provider,
+        transactionsContract})
+}
+
+export const TransactionProvider =({children})=>{
+    return (
+        <TransactionContext.provider>
+            {children}
+        </TransactionContext.provider>)
+
+  };
