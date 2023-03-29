7b05f32a88db8bf27b020e6e9f17374bd008cd01
Haimanotgg <Haimanotgbb@gmail.com>
2023-03-29 11:03:06 +0300

    initial commit

--- Client/src/Context/TransactionContext.jsx
+++ Client/src/Context/TransactionContext.jsx
@@ -7,18 +7,31 @@ export const TransactionContext = React.createContext();
 
 const { ethereum } = window;
 
-const createEthereumContract = () => {
+const getEthereumContract = () => {
     const provider = new ethers.providers.Web3Provider(ethereum);
     const signer = provider.getSigner();
     const transactionsContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-    console.log(
-        {signer,
+    console.log({
+        signer,
         provider,
         transactionsContract})
 }
 
 export const TransactionProvider =({children})=>{
+     const checkIfWalletIsConnected = async () =>{
+        if(!ethereum) return alert ("Please install metamask");
+
+        const accounts = await ethereum.request( {method: 'eth_accounts'});
+
+        console.log(accounts);
+     }
+
+     useEffect(() => {
+       checkIfWalletIsConnected();
+     }, []);
+     
+
     return (
         <TransactionContext.provider value ={{}}>
             {children}
