a7e6372aa6d99ef8fb3c4714ea1b1d23e3dc45bf
Haimanotgg <Haimanotgbb@gmail.com>
2023-03-30 07:14:36 +0300

    initial commit

--- Client/src/Components/Welcome.jsx
+++ Client/src/Components/Welcome.jsx
@@ -22,7 +22,7 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 
 const Welcome = () => {
 
-    const { connectWallet } = useContext(TransactionContext);
+    const { connectWallet, currentAccount} = useContext(TransactionContext);
 
     const handleSubmit = () => {
     
@@ -39,7 +39,7 @@ const Welcome = () => {
                     <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
                     Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
                     </p>
-                    <button
+                    {!currentAccount && (<button
                         type="button"
                         onClick={connectWallet}
                         className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#9546bd]"
@@ -48,7 +48,7 @@ const Welcome = () => {
                         <p className="text-white text-base font-semibold">
                             Connect Wallet
                         </p>
-                    </button>
+                    </button>)}
 
 
                     <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10"> 
