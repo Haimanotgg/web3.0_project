4b8818cd43f865dbac19809710a733cf8b68c342
Haimanotgg <Haimanotgbb@gmail.com>
2023-04-02 15:04:45 +0300

    initial commit

--- Client/src/Components/Services.jsx
+++ Client/src/Components/Services.jsx
@@ -1,10 +1,10 @@
 import { BsShieldFillCheck } from "react-icons/bs";
-import {BiSearchAlt} from "react-icons/bi";
-import {RiHeart2Fill} from 'react-icons/ri'
+import { BiSearchAlt } from "react-icons/bi";
+import { RiHeart2Fill } from 'react-icons/ri';
 
 const ServiceCard = ({color, title, icon, subtitle}) => (
     <div className="flex flex-row justify-start items-center white-glassmorphism p-3 m-2 cursor-pointer hover:shadow-xl">
-        <div className="{w-10 h-10 rounded-full flex justify-center items-center ${color:white}}">
+        <div className={'w-10 h-10 rounded-full flex justify-center items-center + ${color}'}>
             {icon}
         </div>
         <div className="ml-5 flex flex-col flex-1">
@@ -28,22 +28,22 @@ const Services = () => {
             </div>
             <div className="flex-1 flex flex-col justify-start items-center">
                 <ServiceCard  
-                color="bg-[#2952E3]"
-                title="Security Guaranteed"
-                icon= {<BsShieldFillCheck fontSize={21} className="text-white"/>}
-                subtitle ="Security is guaranteed. We always maintain privacy and maintain the quality of our products"
+                    color="bg-[#2952E3]"
+                    title="Security Guaranteed"
+                    icon= {<BsShieldFillCheck fontSize={21} className="text-white"/>}
+                    subtitle ="Security is guaranteed. We always maintain privacy and maintain the quality of our products"
                 />
                  <ServiceCard  
-                color="bg-[#2952E3]"
-                title="Security Guaranteed"
-                icon= {<BiSearchAlt fontSize={21} className="text-white"/>}
-                subtitle ="Security is guaranteed. We always maintain privacy and maintain the quality of our products"
+                    color="bg-[#8945F8]"
+                    title="Best Exchange Rates"
+                    icon= {<BiSearchAlt fontSize={21} className="text-white"/>}
+                    subtitle ="Security is guaranteed. We always maintain privacy and maintain the quality of our products"
                 />
                  <ServiceCard  
-                color="bg-[#2952E3]"
-                title="Security Guaranteed"
-                icon= {<RiHeart2Fill fontSize={21} className="text-white"/>}
-                subtitle ="Security is guaranteed. We always maintain privacy and maintain the quality of our products"
+                    color="bg-[#F84550]"
+                    title="Fastest Transactions"
+                    icon= {<RiHeart2Fill fontSize={21} className="text-white"/>}
+                    subtitle ="Security is guaranteed. We always maintain privacy and maintain the quality of our products"
                 />
             </div>
         </div>
