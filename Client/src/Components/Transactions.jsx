5938a41f4eee7fa77a2d1356ed1e5701de7c0d1a
Haimanotgg <Haimanotgbb@gmail.com>
2023-04-02 23:01:27 +0300

    initial commit

--- Client/src/Components/Transactions.jsx
+++ Client/src/Components/Transactions.jsx
@@ -1,7 +1,17 @@
+import React, {useContext} from 'react';
+import { TransactionContext } from '../Context/TransactionContext';
+// import { DummyData } from '../Context/TransactionContext';
+
 const Transactions = () => {
+    const { cur}
     return (
-        <h1>Transactions</h1>
+       <div className='flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions '>
+            <div className='flex flex-col md:p-12 py-12 px-4'>
+                {/* {Latest Transaction} */}
+                {/* {Connect your account to see your latest transactions} */}
+            </div>
+       </div>
     );
 }
 
-export default Transactions;
+export default Transactions;S
