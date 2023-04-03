5f9ca7c6d2d107c59a03a731177e63d9323cd501
Haimanotgg <Haimanotgbb@gmail.com>
2023-04-03 15:13:23 +0300

    initial commit

--- Client/src/Components/Transactions.jsx
+++ Client/src/Components/Transactions.jsx
@@ -1,14 +1,18 @@
 import React, {useContext} from 'react';
 import { TransactionContext } from '../Context/TransactionContext';
-// import { dummyData } from '../Context/TransactionContext';
+import { dummyData } from '..utils/dummyData' ;
 
 const Transactions = () => {
-    // te
+    const { currentAccount } = useContext (TransactionContext);
     return (
-       <div className='flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions '>
+       <div className='flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions'>
             <div className='flex flex-col md:p-12 py-12 px-4'>
-                {/* {Latest Transaction} */}
-                {/* {Connect your account to see your latest transactions} */}
+                {currentAccount? (
+                    <h3 className="text-whit text-3xl text-center my-2">Latest Transactions</h3>
+                ) : (
+                    <h3 className="text-whit text-3xl text-center my-2"> Connect your account to see your latest transactions </h3>
+                )}
+
             </div>
        </div>
     );
